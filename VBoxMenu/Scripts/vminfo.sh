2ca06d974caee226d8ee1b49d357ce3886cdf657
Francesc Vila <francesc.vila@gmail.com>
2012-08-16 15:44:12 +0200

    Changed script to recognize windows icons

--- VBoxMenu/Scripts/vminfo.sh
+++ VBoxMenu/Scripts/vminfo.sh
@@ -6,4 +6,4 @@
 #  Created by Francesc Vila on 07/08/2012.
 #  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 
-VBoxManage showvminfo "$1" --machinereadable | grep ostype | cut -d\" -f2 | tr '[A-Z]' '[a-z]' | tr -d '\n'
+VBoxManage showvminfo "$1" --machinereadable | grep ostype | cut -d\" -f2 | tr '[A-Z]' '[a-z]' | sed 's/windows/win/g' | tr -d '\n'
